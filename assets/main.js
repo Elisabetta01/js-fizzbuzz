e58597f74f418ddd45b9198ab8de14a032d07c2c
Elisabetta01 <scagione.elisabetta@gmail.com>
2023-03-28 17:38:26 +0200

    creazione div

--- assets/main.js
+++ assets/main.js
@@ -2,16 +2,38 @@
 
 // stampo in console i numeri da 1 a 100 con condizioni
 
+let container = document.getElementById('container');
+
+
+
 for( let i = 1; i <= 100; i++){
+
+     let div = document.createElement('div');
 
      // condizioni
      if( i % 3 == 0 && i % 5 == 0 ){
-          console.log("FizzBuzz")
+          div.className = 'box box-red'
+          container.append( div )
+          div.innerHTML = "fizzbuzz"
+
+          console.log("fizzbuzz")
      } else if ( i % 3 == 0 ){
-          console.log("Fizz")
+          div.className = 'box box-green'
+          container.append( div )
+          div.innerHTML = "fizz"
+
+          console.log("fizz")
      } else if ( i % 5 == 0 ){
-          console.log("Buzz")
+          div.className = 'box box-yellow'
+          container.append( div )
+          div.innerHTML = "buzz"
+
+          console.log("buzz")
      } else {
+          div.className = 'box'
+          container.append( div )
+          div.innerHTML = i
+
           console.log( i )
      }
 
